8185df44d0af5317758a4cb3d94ca40fba52dd17
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-02-04 14:33:14 -0400

    Eliminando el constructor de la clase Usuarios ya que no es necesario. Closes #9.

--- Entidades/Usuarios.cs
+++ Entidades/Usuarios.cs
@@ -16,17 +16,5 @@ namespace ProyectoNuevoRegistro.Entidades
         public string Email { get; set; }
         public string DescripcionRol { get; set; }
         public bool Activo { get; set; }
-
-       /* public Usuarios()
-        {
-            UsuarioId = 0;
-            Clave = string.Empty;
-            FechaIngreso = DateTime.Now;
-            Alias = string.Empty;
-            Nombres = string.Empty;
-            Email = string.Empty;
-            DescripcionRol = string.Empty;
-            Activo = false;
-        }*/
     }
 }
